b7276cb3d1004b20c20670697d83e73539045427
Sahilshrestha1298 <shresthasahil24@gmail.com>
2026-02-12 10:12:20 +0545

    updating the app js again

--- js/app.js
+++ js/app.js
@@ -58,3 +58,55 @@ function addItem(itemName) {
     alert("Item Added Successfully!");
   }, 0);
 }
+
+// ....
+
+var editId = null;
+
+// Render App
+function render() {
+  var $app = $("#app");
+  $app.empty();
+
+  var itemToEdit = editId
+    ? $.grep(items, function (item) {
+        return item.id === editId;
+      })[0]
+    : null;
+  var $formElement = createForm(editId, itemToEdit); // edited line
+  var $itemsElement = createItems(items);
+
+  $app.append($formElement);
+  $app.append($itemsElement);
+}
+
+// Initialize App
+$(document).ready(function () {
+  render();
+});
+
+// Update Item Name Function
+function updateItemName(newName) {
+  items = $.map(items, function (item) {
+    if (item.id === editId) {
+      return $.extend({}, item, { name: newName });
+    }
+    return item;
+  });
+  editId = null;
+  render();
+  setTimeout(function () {
+    alert("Item Updated Successfully!");
+  }, 0);
+}
+
+// Set Edit ID Function
+function setEditId(itemId) {
+  editId = itemId;
+  render();
+
+  // Focus input after render
+  setTimeout(function () {
+    $(".form-input").focus();
+  }, 0);
+}
